c2fff9b7b22b300e927ff38176bd2e5cde825dcf
R7600\watsk <watskmt@gmail.com>
2026-04-15 10:28:42 +0900

    画面初期化

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "DxLib.h"
 #define WIDTH 10
 #define HEIGHT 10
-#define WINDOW_WIDTH 400
-#define WINDOW_HEIGHT 800
+#define CELL_SIZE 40
+#define MARGIN_X 100
+#define MARGIN_Y 10
+#define WINDOW_WIDTH CELL_SIZE * WIDTH+MARGIN_X * 2
+#define WINDOW_HEIGHT CELL_SIZE * HEIGHT+MARGIN_Y * 2
 
 // Point 定義
 struct Point
@@ -12,10 +15,16 @@ struct Point
 };
 Point ConvertTopLeftToBottomLeft(Point p);
 
+void initializeDrawScreen(void);
+void DrawCell(int x, int y);
+Point GameBoardToScreen(Point p);
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+    int cellSizeX = WINDOW_WIDTH / WIDTH;
+    int cellSizeY = WINDOW_HEIGHT / HEIGHT;
     ChangeWindowMode(TRUE);
-    SetGraphMode(640, 480, 32);
+    SetGraphMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32);
     SetMainWindowText(L"DXLib Project");
 
     if (DxLib_Init() == -1)
@@ -29,13 +38,37 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     {
         ClearDrawScreen();
 
-        DrawString(10, 10, L"Hello, DXLib!", GetColor(255, 255, 255));
+		initializeDrawScreen();
 
         ScreenFlip();
     }
 
     DxLib_End();
     return 0;
+}
+
+void initializeDrawScreen(void)
+{
+    for (int y = 0; y < HEIGHT; y++)
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            DrawCell(x, y);
+        }
+    }
+}
+
+// DrawCell: 指定した盤面座標のセルを画面に描画する。
+//    - 入力: Point p (x, y は左下基準のセル番号)
+//    - 出力: なし
+void DrawCell(int x, int y)
+{
+    Point start = {x, y};
+	Point end = { x + 1, y + 1 };
+    Point converted_start = GameBoardToScreen(start);
+    Point converted_end = GameBoardToScreen(end);
+
+    DrawBox(converted_start.x, converted_start.y, converted_end.x - 1, converted_end.y - 1, GetColor(255, 255, 255), FALSE);
 }
 
 
@@ -52,5 +85,18 @@ Point ConvertTopLeftToBottomLeft(Point p)
     // 上下反転：描画領域の高さが WINDOW_HEIGHT の場合、y = 0 は最上部、
     // 反転後は WINDOW_HEIGHT-1
     r.y = (WINDOW_HEIGHT - 1) - p.y;
+    return r;
+}
+
+// 盤面座標（セル座標） -> スクリーン座標 に変換する関数
+//    入力：Point p　セル座標
+// 　　出力：そのセルの左下角のスクリーン座標
+Point GameBoardToScreen(Point p)
+{
+    Point r;
+    r.x = p.x * CELL_SIZE + MARGIN_X;
+    r.y = p.y * CELL_SIZE + MARGIN_Y;
+
+	r = ConvertTopLeftToBottomLeft(r);
     return r;
 }
