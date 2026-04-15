30813bd03d67b919ec68a2fb9eac337f77267343
R7600\watsk <watskmt@gmail.com>
2026-04-15 09:25:26 +0900

    座標変換関数（左下原点の座標を渡して、実際の描画座標（左上原点）を返す）

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
 #include "DxLib.h"
+#define WIDTH 10
+#define HEIGHT 10
+#define WINDOW_WIDTH 400
+#define WINDOW_HEIGHT 800
+
+// Point 定義
+struct Point
+{
+    int x;
+    int y;
+};
+Point ConvertTopLeftToBottomLeft(Point p);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -24,4 +36,21 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     DxLib_End();
     return 0;
+}
+
+
+// 左上原点 (0,0 が左上) を左下原点 (0,0 が左下) に変換する関数を定義する。
+//    - 入力: Point p (x, y は左上原点座標)
+//    - 出力: Point r (x は変化なし、y は上下反転された値)
+//    - 変換式: r.y = (WINDOW_HEIGHT_ - 1) - p.y
+// 左上原点 -> 左下原点 に変換する関数
+
+Point ConvertTopLeftToBottomLeft(Point p)
+{
+    Point r;
+    r.x = p.x;
+    // 上下反転：描画領域の高さが WINDOW_HEIGHT の場合、y = 0 は最上部、
+    // 反転後は WINDOW_HEIGHT-1
+    r.y = (WINDOW_HEIGHT - 1) - p.y;
+    return r;
 }
